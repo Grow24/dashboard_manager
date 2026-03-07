fe51629967c21df12a00d45c4675ef372d4dedb0
Bappu Kumar <bappu@example.com>
2026-03-07 11:55:15 +0530

    css issue new

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX ?? "";
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-
 const nextConfig: NextConfig = {
-  ...(assetPrefix ? { assetPrefix } : {}),
-  ...(basePath ? { basePath } : {}),
   typescript: {
     ignoreBuildErrors: true,
   },
